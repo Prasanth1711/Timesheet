822276a8f8d9cef8a9a30fb14d54317b1f09dbc7
Prasanth kumar <nprasanth.etl@gmail.com>
2024-02-29 01:05:10 +0530

    added the daterangepicker

--- app/components/TimeSheetHome.tsx
+++ app/components/TimeSheetHome.tsx
@@ -78,7 +78,7 @@ export const TimeSheetHome = () => {
         <div>
             
 <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-black bg-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center  dark:focus:ring-blue-800" type="button">Dropdown button <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
 </svg>
 </button>
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+"use client"
 import {TimeSheetHome} from './components/TimeSheetHome'
+import { DatePickerWithRange } from "./components/Temp";
 export default function Home() {
   return (
     <main className="w-screen h-screen bg-gray-200">
     <TimeSheetHome/>
+    <DatePickerWithRange/>
     </main>
   );
 }
