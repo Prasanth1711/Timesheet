7f4a4b18219ef7a31840d6587939a239ec8e4bbb
Prasanth kumar <nprasanth.etl@gmail.com>
2024-03-02 17:43:13 +0530

    Hydration Error removedc

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,4 +34,5 @@ export default function RootLayout({
         {children}</body>
     </html>
   );
-}
+
+  }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
 "use client"
 import {TimeSheetHome} from './components/TimeSheetHome'
 import { DatePickerWithRange } from "./components/Temp";
-export default function Home() {
+import { useEffect, useState } from 'react';
+const Home=() =>{
   return (
+    <Hydrated>
     <main className="w-screen h-screen bg-gray-200">
     <TimeSheetHome/>
-    <DatePickerWithRange/>
     </main>
+    </Hydrated>
   );
 }
+const Hydrated = ({ children }: { children?: any }) => {
+  const [hydration, setHydration] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setHydration(true)
+    }
+  }, [])
+  return hydration ? children : null
+}
+
+export default Home
